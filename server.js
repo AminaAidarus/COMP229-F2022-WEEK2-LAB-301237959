3759955e82f92f7489a635fc03712b8eb7c9a195
AminaAidarus <aaidaru1@my.centennialcollege.ca>
2022-09-17 19:42:45 -0400

    server.js

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const connect = require('connect');
 //instantiate app-server
 const app = connect();
 
-function logger(req, res, next){
-    console.log(req.method, req.url);
-    next();
-}
 //custom middleware
 function helloPlain(req, res, next){
     res.setHeader('Content-Type','text/plain');
     res.end("Hello from NodeJS Application");
-    next();
 }
 
 //custom middleware
@@ -28,11 +23,11 @@ function helloJson(req, res, next){
 }
 
 //add middleware to connect application
-app.use(helloPlain);
+app.use('/', helloPlain);
 app.use('/html', helloHtml);
 app.use('/json', helloJson);
 
 //run app
 app.listen(3000);
 
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3000');
