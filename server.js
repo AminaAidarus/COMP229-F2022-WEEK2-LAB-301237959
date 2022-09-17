ae99ea3b421f7450e798a577aedb4798c58234d9
AminaAidarus <aaidaru1@my.centennialcollege.ca>
2022-09-17 18:53:47 -0400

    new file:   node_modules/.package-lock.json 	new file:   node_modules/connect/HISTORY.md 	new file:   node_modules/connect/LICENSE 	new file:   node_modules/connect/README.md 	new file:   node_modules/connect/SECURITY.md 	new file:   node_modules/connect/index.js 	new file:   node_modules/connect/package.json 	new file:   node_modules/debug/.coveralls.yml 	new file:   node_modules/debug/.eslintrc 	new file:   node_modules/debug/.npmignore 	new file:   node_modules/debug/.travis.yml 	new file:   node_modules/debug/CHANGELOG.md 	new file:   node_modules/debug/LICENSE 	new file:   node_modules/debug/Makefile 	new file:   node_modules/debug/README.md 	new file:   node_modules/debug/component.json 	new file:   node_modules/debug/karma.conf.js 	new file:   node_modules/debug/node.js 	new file:   node_modules/debug/package.json 	new file:   node_modules/debug/src/browser.js 	new file:   node_modules/debug/src/debug.js 	new file:   node_modules/debug/src/index.js 	new file:   node_modules/debug/src/inspector-log.js 	new file:   node_modules/debug/src/node.js 	new file:   node_modules/ee-first/LICENSE 	new file:   node_modules/ee-first/README.md 	new file:   node_modules/ee-first/index.js 	new file:   node_modules/ee-first/package.json 	new file:   node_modules/encodeurl/HISTORY.md 	new file:   node_modules/encodeurl/LICENSE 	new file:   node_modules/encodeurl/README.md 	new file:   node_modules/encodeurl/index.js 	new file:   node_modules/encodeurl/package.json 	new file:   node_modules/escape-html/LICENSE 	new file:   node_modules/escape-html/Readme.md 	new file:   node_modules/escape-html/index.js 	new file:   node_modules/escape-html/package.json 	new file:   node_modules/finalhandler/HISTORY.md 	new file:   node_modules/finalhandler/LICENSE 	new file:   node_modules/finalhandler/README.md 	new file:   node_modules/finalhandler/index.js 	new file:   node_modules/finalhandler/package.json 	new file:   node_modules/ms/index.js 	new file:   node_modules/ms/license.md 	new file:   node_modules/ms/package.json 	new file:   node_modules/ms/readme.md 	new file:   node_modules/on-finished/HISTORY.md 	new file:   node_modules/on-finished/LICENSE 	new file:   node_modules/on-finished/README.md 	new file:   node_modules/on-finished/index.js 	new file:   node_modules/on-finished/package.json 	new file:   node_modules/parseurl/HISTORY.md 	new file:   node_modules/parseurl/LICENSE 	new file:   node_modules/parseurl/README.md 	new file:   node_modules/parseurl/index.js 	new file:   node_modules/parseurl/package.json 	new file:   node_modules/statuses/HISTORY.md 	new file:   node_modules/statuses/LICENSE 	new file:   node_modules/statuses/README.md 	new file:   node_modules/statuses/codes.json 	new file:   node_modules/statuses/index.js 	new file:   node_modules/statuses/package.json 	new file:   node_modules/unpipe/HISTORY.md 	new file:   node_modules/unpipe/LICENSE 	new file:   node_modules/unpipe/README.md 	new file:   node_modules/unpipe/index.js 	new file:   node_modules/unpipe/package.json 	new file:   node_modules/utils-merge/.npmignore 	new file:   node_modules/utils-merge/LICENSE 	new file:   node_modules/utils-merge/README.md 	new file:   node_modules/utils-merge/index.js 	new file:   node_modules/utils-merge/package.json 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+//impoting third-party connect module
+const conect = require('connect');
+
+//instantiate app-server
+const app = connect();
+
+//custom middleware
+function helloPlain(req, res, next){
+    res.setHeader('Content-Type','text/plain');
+    res.end("Hello from NodeJS Application");
+}
+
+//custom middleware
+function helloHtml(req, res, next){
+    res.setHeader('Content-Type','text/html');
+    res.end("<h1>Hello from NodeJS Application as html</h1>");
+}
+
+//custom middleware
+function helloJson(req, res, next){
+    res.setHeader('Content-Type','application/json');
+    res.end('{"message":"Hello from NodeJS Application as json"}');
+}
+
+//add middleware to connect application
+app.use('/', helloPlain);
+app.use('/html', helloHtml);
+app.use('/json', helloJson);
+
+//run app
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000');
