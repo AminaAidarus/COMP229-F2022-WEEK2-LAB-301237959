b11d749351f51b45d4014a6e129469bf71f266a1
AminaAidarus <aaidaru1@my.centennialcollege.ca>
2022-09-17 19:25:57 -0400

    server.js

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ function logger(req, res, next){
 //custom middleware
 function helloPlain(req, res, next){
     res.setHeader('Content-Type','text/plain');
+    next();
     res.end("Hello from NodeJS Application");
 }
 
 //custom middleware
 function helloHtml(req, res, next){
     res.setHeader('Content-Type','text/html');
+    next();
     res.end("<h1>Hello from NodeJS Application as html</h1>");
 }
 
 //custom middleware
 function helloJson(req, res, next){
     res.setHeader('Content-Type','application/json');
+    next();
     res.end('{"message":"Hello from NodeJS Application as json}');
 }
 
